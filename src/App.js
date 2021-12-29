45e7a16dad367831e60275147236cfa49858b8d8
AnumMujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-12-29 23:52:18 +0500

    feat(App.js): read data from firebase realtime database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase/app';
+import { config } from './firebaseConfig';
+import React, { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
+firebase.initializeApp(config);
+const db = getDatabase();
 function App() {
+  // Define the state of the component
+  const [bpm, setBpm] = useState(0);
+
+  // Listen to changes on the firebase database, specifically the "distance" entry
+  useEffect(() => {
+    const starCountRef = ref(db, 'bpm');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      // updateStarCount(postElement, data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{bpm}</p>
     </div>
   );
 }
